7050dd9dfca34edfb2f095f325792b33204cadb0
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2022-01-29 17:36:27 -0500

    add proper testing (cover only `is_stable_method` so far)

--- src/StabilityCheck.jl
+++ src/StabilityCheck.jl
@@ -4,12 +4,14 @@ module StabilityCheck
 # Exhaustive enumeration of types for static type stability checking
 #
 
-export @stable, is_stable_method, is_stable_function
+export @stable, is_stable_method, is_stable_function,
+    Stb, Uns
 
 # Debug print:
 # ENV["JULIA_DEBUG"] = Main    # turn on
 # ENV["JULIA_DEBUG"] = Nothing # turn off
 
+using InteractiveUtils
 using MacroTools
 
 abstract type StCheck end
@@ -196,73 +198,5 @@ is_concrete_type(@nospecialize(ty)) = begin
     #         Union{Int,Missing}; those are deemed "probably
     #         harmless"
 end
-
-#
-# Examples of type-(un)stable functions
-#
-
-abstract type MyAbsVec{T} end
-
-struct MyVec{T <: Signed} <: MyAbsVec{T}
-    data :: Vector{T}
-end
-
-# Ex. (mysum1) stable, hard: abstract parametric type
-mysum1(a::AbstractArray) = begin
-    r = zero(eltype(a))
-    for x in a
-        r += x
-    end
-    r
-end
-
-# Ex. (mysum2) stable, hard: cf. (mysum1) but use our types
-# for simpler use case
-mysum2(a::MyAbsVec) = begin
-    r = zero(eltype(a))
-    for x in a
-        r += x
-    end
-    r
-end
-
-# Ex. (add1)
-# |
-# --- a) using `one` -- should be stable but alas, the Rational{Bool} instance screws it
-add1i(x :: Integer) = x + one(x)
-# |
-# --- b) using `1` -- surpricingly stable (coercion)
-add1iss(x :: Integer) = x + 1
-# |
-# --- c) with type inspection -- still stable! (constant folding)
-add1uns(x :: Number) =
-    if typeof(x) <: Integer
-        x + 1
-    elseif typeof(x) <: Real
-        x + 1.0
-    else
-        x + one(x)
-    end
-# |
-# -- d) Number-input: lots of subtypes. Would be stable if skip Rational{Bool} and
-#       abstract arguments to parametric types (e.g. Complex{Integer}).
-#       Currently unstable.
-add1n(x :: Number) = x + one(x)
-
-trivial_unstable(x::Int) = x > 0 ? 0 : "0"
-
-plus(x :: Number, y :: Number) = x + y
-
-sum_top(v, t) = begin
-    res = 0 # zero(eltype(v))
-    for x in v
-        res += x < t ? x : t
-    end
-    res
-end
-
-# test call:
-#is_stable_function(add1uns)
-# TODO: fails on `plus` (likely, due to >1 args)
 
 end # module
---
+++ test/runtests.jl
@@ -0,0 +1,85 @@
+using Test
+using InteractiveUtils
+using StabilityCheck
+
+#
+# Examples of type-(un)stable functions
+#
+
+abstract type MyAbsVec{T} end
+
+struct MyVec{T <: Signed} <: MyAbsVec{T}
+    data :: Vector{T}
+end
+
+# Ex. (mysum1) stable, hard: abstract parametric type
+mysum1(a::AbstractArray) = begin
+    r = zero(eltype(a))
+    for x in a
+        r += x
+    end
+    r
+end
+
+# Ex. (mysum2) stable, hard: cf. (mysum1) but use our types
+# for simpler use case
+mysum2(a::MyAbsVec) = begin
+    r = zero(eltype(a))
+    for x in a
+        r += x
+    end
+    r
+end
+
+# Ex. (add1)
+# |
+# --- a) using `one` -- should be stable but alas, the Rational{Bool} instance screws it
+add1i(x :: Integer) = x + one(x)
+# |
+# --- b) using `1` -- surpricingly stable (coercion)
+add1iss(x :: Integer) = x + 1
+# |
+# --- c) with type inspection -- still stable! (constant folding)
+add1uns(x :: Number) =
+    if typeof(x) <: Integer
+        x + 1
+    elseif typeof(x) <: Real
+        x + 1.0
+    else
+        x + one(x)
+    end
+# |
+# -- d) Number-input: lots of subtypes. Would be stable if skip Rational{Bool} and
+#       abstract arguments to parametric types (e.g. Complex{Integer}).
+#       Currently unstable.
+add1n(x :: Number) = x + one(x)
+
+trivial_unstable(x::Int) = x > 0 ? 0 : "0"
+
+plus(x :: Number, y :: Number) = x + y
+
+sum_top(v, t) = begin
+    res = 0 # zero(eltype(v))
+    for x in v
+        res += x < t ? x : t
+    end
+    res
+end
+
+# test call:
+#is_stable_function(add1uns)
+# TODO: fails on `plus` (likely, due to >1 args)
+
+#
+# Tests
+#
+
+@testset "Simple stable" begin
+    @test is_stable_method(@which add1i(1))   == Stb()
+    @test is_stable_method(@which add1iss(1)) == Stb()
+end
+
+@testset "Simple unstable" begin
+    @test isa(is_stable_method(@which add1uns(1)), Uns)
+    @test isa(is_stable_method(@which add1n(1)),   Uns)
+end
