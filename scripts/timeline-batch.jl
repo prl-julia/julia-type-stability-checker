7faa366c5da4ccc8c79f02c02f6eb5798d885b59
Jan Ječmen <jan.jecmen@gmail.com>
2023-04-21 14:33:22 +0000

    Batch script to process multiple packages
    It processes each of the listed packages with the timeline tool and aggregates the results.

---
+++ scripts/timeline-batch.jl
@@ -0,0 +1,54 @@
+#
+# Process a batch of Julia packages.
+#
+# Usage: Run with `julia --threads=<N> <path/to/julia-sts>/scripts/timeline-batch.jl`
+#        It's recommended to redirect output to a log file (e.g. `&> LOG` in bash) since there is a lot of ouput
+# Effect: results are stored in the CWD in `timelines` subdirectory, temporary intermediate files are in `scratch` subdirectory
+#
+
+sts_path = dirname(@__DIR__)
+
+function info(s::String)
+    @info s
+    flush(stdout)
+    flush(stderr)
+end
+
+packages = [
+    ("Multisets", "https://github.com/scheinerman/Multisets.jl.git"),
+    ("Gen", "https://github.com/probcomp/Gen.jl.git"),
+    ("Flux", "https://github.com/FluxML/Flux.jl.git"),
+    ("Gadfly", "https://github.com/GiovineItalia/Gadfly.jl.git"),
+    ("Genie", "https://github.com/GenieFramework/Genie.jl.git"),
+    ("IJulia", "https://github.com/JuliaLang/IJulia.jl.git"),
+    ("JuMP", "https://github.com/jump-dev/JuMP.jl.git"),
+    ("Knet", "https://github.com/denizyuret/Knet.jl.git"),
+    ("Plots", "https://github.com/JuliaPlots/Plots.jl.git"),
+    ("Pluto", "https://github.com/fonsp/Pluto.jl.git"),
+]
+
+# Use 2/3 of available threads
+n = Threads.nthreads()
+info("== Using $n threads ==")
+
+for (pkg, url) in packages
+    _, t = @timed begin
+        scratch = joinpath(".", "scratch/$pkg")
+        out = joinpath(".", "timelines/$pkg.csv")
+        info("=== Checking `$pkg' ===")
+        run(`julia --threads=$n $(joinpath(sts_path, "scripts/timeline.jl")) $url $scratch`)
+        info("=== Aggregating results for `$pkg' ===")
+        run(`julia $(joinpath(sts_path, "scripts/timeline-aggregate.jl")) $scratch $out`)
+    end
+    # Report nice time
+    t, unit = t, "s"
+    if t > 60
+        t /= 60
+        unit = "m"
+    end
+    if t > 60
+        t /= 60
+        unit = "h"
+    end
+    info("=== Done with $pkg in $(round(t, digits=3))$unit")
+end
