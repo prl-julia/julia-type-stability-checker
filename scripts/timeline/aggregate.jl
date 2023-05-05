4ec128646202ef8d5575027c04d5671cf9ce132b
Jan Ječmen <jan.jecmen@gmail.com>
2023-05-05 15:21:13 +0000

    Rework the aggregate script
    Use the new format of the package info generated by the history script

--- scripts/timeline/aggregate.jl
+++ scripts/timeline/aggregate.jl
@@ -8,45 +8,27 @@
 in_dir = length(ARGS) > 0 ? ARGS[1] : error("Requires argument: input directory")
 out_file = length(ARGS) > 1 ? ARGS[2] : error("Requires argument: output file")
 
-function exec(cmd::Cmd)
-    out = IOBuffer()
-    err = IOBuffer()
-    run(pipeline(cmd, stdout=out, stderr=err))
-    strip(String(take!(out)) * String(take!(err)))
-end
+include("utils.jl")
 
 all_agg_files = sort(split(exec(`find $in_dir -maxdepth 2 -name "*-agg.txt"`), "\n"))
 
 if length(all_agg_files) == 0 || (length(all_agg_files) == 1 && first(all_agg_files) == "")
-    @info "No aggregate files found..."
+    @info_extra "No aggregate files found..."
     exit(0)
 end
 
-result = ["Commit,Message,Date,Version,Module,Methods,Stable,Partial,Unstable,Any,Vararg,Generic,TcFail,NoFuel"]
+result = ["Module,Methods,Stable,Partial,Unstable,Any,Vararg,Generic,TcFail,NoFuel,Version,Commit,Message,Date"]
 last_changed_row = ""
 for agg in all_agg_files
     dir = dirname(agg)
-    commit = strip(read(joinpath(dir, "timeline_commit.txt"), String))
-    msg = strip(read(joinpath(dir, "timeline_msg.txt"), String))
-    when = strip(read(joinpath(dir, "timeline_when.txt"), String))
-    version = strip(read(joinpath(dir, "timeline_version.txt"), String))
+    info = read_strip(joinpath(dir, "timeline_info.csv"))
+    data = read_strip(agg)
 
-    # csv escapes
-    msg = replace(msg, '"' => "\"\"")
-
-    data = strip(read(agg, String))
     # only report lines that are different from the last line
     if data != last_changed_row
         global last_changed_row = data
-        push!(result, "$commit,\"$msg\",$when,$version,$data")
+        push!(result, "$data,$info")
     end
 end
 
-if !isdir(dirname(out_file))
-    mkpath(dirname(out_file))
-end
-@info "Writing to `$out_file'"
-open(out_file, "w") do f
-    write(f, join(result, "\n"))
-    write(f, "\n")
-end
+dump(out_file, join(result, "\n") * "\n")
