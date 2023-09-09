ae53bd023ea0effa4747beb338c052c070cdc4d8
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2023-09-09 07:01:33 -0400

    reporting: fix heading of the resulting aggregate CSV

--- scripts/aggregate.sh
+++ scripts/aggregate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-ADD_HEAD='1i Module,MethodsGood,Stable,Unstable,NoFuel,DBStableDiff,DBUnstableDiff,DBNoFuelDiff'
+ADD_HEAD='1i Module,MethodsGood,Stable,Unstable,NoFuel,DBStable,DBUnstable,DBNoFuel'
 
 find . -maxdepth 2 -name "*-agg.txt" -exec cat {} + | sort | sed "$ADD_HEAD" > aggregate.csv
