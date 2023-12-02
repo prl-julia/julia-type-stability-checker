1c3fc37d0978d19d37d911ca76f575660fb72e71
Jan Ječmen <jan.jecmen@gmail.com>
2023-12-02 20:37:45 +0000

    rework and fixes

--- src/StabilityCheck.jl
+++ src/StabilityCheck.jl
@@ -215,90 +215,170 @@ end
 
 # --------------------------------------------------------------------------------------------------------------------------------
 
-
-
-
-struct OpaqueFunction
-    f::Function
+struct OpaqueDiscovery
+    x::Union{Function, Tuple{Module, Symbol, Type}}
 end
-struct OpaqueMethod
+
+
+
+
+abstract type Discovery end
+abstract type FunctionDiscovery end
+abstract type TypeDiscovery end
+
+struct DiscoveredMacro <: FunctionDiscovery
     f::Function
     m::Method
 end
-struct OpaqueType
+struct DiscoveredLambda <: FunctionDiscovery
+    f::Function
+    m::Method
+end
+struct DiscoveredConstructor <: FunctionDiscovery
+    f::Function
+    m::Method
+end
+struct DiscoveredCallable <: FunctionDiscovery
+    f::Function
+    m::Method
+end
+struct DiscoveredBuiltin <: FunctionDiscovery
+    f::Function
+    m::Method
+end
+struct DiscoveredIntrinsic <: FunctionDiscovery
+    f::Function
+    m::Method
+end
+struct DiscoveredGeneric <: FunctionDiscovery
+    f::Function
+    m::Method
+end
+
+struct DiscoveredAlias <: TypeDiscovery
+    m::Module
     s::Symbol
     t::Type
+end
+struct DiscoveredClosure <: TypeDiscovery
     m::Module
+    s::Symbol
+    t::Type
+end
+struct DiscoveredFunctionType <: TypeDiscovery
+    m::Module
+    s::Symbol
+    t::Type
+end
+struct DiscoveredType <: TypeDiscovery
+    m::Module
+    s::Symbol
+    t::Type
 end
 
-discover(mod::Module, root::Module, seen::Set{Module}, functions::Set{OpaqueFunction}, types::Set{OpaqueType}) = begin
-    mod ∈ seen && return
-    push!(seen, mod)
 
-    for sym in names(mod; all=true, imported=true)
-        try
-            evsym = getproperty(mod, sym)
+discover(report::Function, modules::Vector{Module}) = begin
+    visited = Set{Module}()
+    discovered = Set{OpaqueDiscovery}()
 
-            if evsym isa Module && is_module_nested(evsym, root)
-                discover(evsym, root, seen, functions, types)
-                continue
-            end
-
-            if evsym isa Function && sym ∉ [:include, :eval] #  && !(val isa Core.Builtin) && !(val isa Core.IntrinsicFunction)
-                push!(functions, OpaqueFunction(evsym))
-                continue
-            end
-
-            if (evsym isa DataType && !(evsym <: Function)) || evsym isa UnionAll
-                push!(types, OpaqueType(sym, evsym, mod))
-                continue
-            end
-
-        catch e
-            if e isa UndefVarError
-                GlobalRef(mod, sym) ∉ [GlobalRef(Base, :active_repl), GlobalRef(Base, :active_repl_backend),
-                                       GlobalRef(Base.Filesystem, :JL_O_TEMPORARY), GlobalRef(Base.Filesystem, :JL_O_SHORT_LIVED),
-                                       GlobalRef(Base.Filesystem, :JL_O_SEQUENTIAL), GlobalRef(Base.Filesystem, :JL_O_RANDOM)] &&
-                    @warn "Module $mod exports symbol $sym but it's undefined."
+    makefuncdiscovery(f::Function, m::Method) = begin
+        f isa Core.Builtin && return DiscoveredBuiltin(f, m)
+        f isa Core.IntrinsicFunction && return DiscoveredIntrinsic(f, m)
+        mt = typeof(f).name.mt
+        name = mt.name
+        hasname = isdefined(mt.module, name) && typeof(getfield(mt.module, name)) <: Function
+        sname = string(name)
+        if hasname
+            if startswith(sname, '@')
+                return DiscoveredMacro(f, m)
             else
-                throw(e)
+                return DiscoveredGeneric(f, m)
+            end
+        elseif '#' in sname
+            return DiscoveredLambda(f, m)
+        elseif mt === Base._TYPE_NAME.mt
+            return DiscoveredConstructor(f, m)
+        else
+            return DiscoveredCallable(f, m)
+        end
+    end
+    maketypediscovery(m::Module, s::Symbol, t::Type) = begin
+        base = Base.unwrap_unionall(t)
+        if t <: Function
+            if occursin("var\"#", string(base))
+                return DiscoveredClosure(m, s, t)
+            else
+                return DiscoveredFunctionType(m, s, t)
+            end
+        elseif base isa Union || t !== base.name.wrapper || string(s) != string(nameof(t))
+            return DiscoveredAlias(m, s, t)
+        else
+            return DiscoveredType(m, s, t)
+        end
+    end
+
+    discoveraux(mod::Module, root::Module) = begin
+        mod ∈ visited && return
+        push!(visited, mod)
+    
+        for sym in names(mod; all=true, imported=true)
+            try
+                val = getproperty(mod, sym)
+    
+                if val isa Module && is_module_nested(val, root)
+                    discoveraux(val, root)
+                    continue
+                end
+    
+                if val isa Function && sym ∉ [:include, :eval]
+                    d = OpaqueDiscovery(val)
+                    d ∈ discovered && continue
+                    push!(discovered, d)
+                    for m in methods(val)
+                        any(mod -> is_module_nested(m.module, mod), modules) && report(makefuncdiscovery(val, m))
+                    end
+                end
+
+                if val isa Type && !(val isa Core.TypeofBottom)
+                    d = OpaqueDiscovery((mod, sym, val))
+                    d ∈ discovered && continue
+                    push!(discovered, d)
+                    report(maketypediscovery(mod, sym, val))
+                end
+
+            catch e
+                if e isa UndefVarError
+                    GlobalRef(mod, sym) ∉ [GlobalRef(Base, :active_repl), GlobalRef(Base, :active_repl_backend),
+                                           GlobalRef(Base.Filesystem, :JL_O_TEMPORARY), GlobalRef(Base.Filesystem, :JL_O_SHORT_LIVED),
+                                           GlobalRef(Base.Filesystem, :JL_O_SEQUENTIAL), GlobalRef(Base.Filesystem, :JL_O_RANDOM)] &&
+                        @warn "Module $mod exports symbol $sym but it's undefined."
+                else
+                    throw(e)
+                end
             end
         end
     end
-end
-
-declarationsAndSignatures(report::Function, modules::Vector{Module}) = begin
-    functions = Set{OpaqueFunction}()
-    types = Set{OpaqueType}()
-    visited = Set{Module}()
 
     for m in modules
-        discover(m, m, visited, functions, types)
+        discoveraux(m, m)
     end
 
-    for f in functions
-        for m in methods(f.f)
-            any(mod -> is_module_nested(m.module, mod), modules) && report(OpaqueMethod(f.f, m))
-        end
-    end
-
-    for t in types
-        report(t)
-    end
 end
 
 module TestMod
 
-    import SentinelArrays
-    const SVec{T} = SentinelArrays.SentinelVector{T, T, Missing, Vector{T}}
+    # import SentinelArrays
+    # const SVec{T} = SentinelArrays.SentinelVector{T, T, Missing, Vector{T}}
 
     # gener(captured) = [captured + i for i in 1:2]
 
     # f(abc) = x -> abc + x
 
     # const lambda = (x::Int32) -> x + 1
-        
+
     # const MyVector{T} = Array{T,1}
+    # const MyVector2 = Vector
+    # const MyVector3 = Vector{T} where T
     # const MyVectorInt = Vector{Int}
     # struct CrazyArray{A, B, C, D, E} end
     # const MyCrazyArray{T, U} = CrazyArray{T, U, Int, Bool, 3}
@@ -309,10 +389,14 @@ module TestMod
     # struct X{T}
     #     x::T
     # end
-
-    # function (x::X)(a)
+    # function (x::X)(a) # X.body.name.mt has callable objects (one mt for all of them??)
     #     return x.x + a
     # end
+
+    struct K
+        k::Int
+    end
+    K(a, b, c) = K(a + b + c)
 
     # abstract type TestAbstract end
 
@@ -336,42 +420,137 @@ module TestMod
     # foo(::Vector{T}, ::T) where T <: Number = 1
 
     # module Submod
+        # const MyVector2 = Vector
 
-    #     struct Substruct
-    #         x::Int
-    #     end
+        # struct Substruct
+        #     x::Int
+        # end
 
-    #     subfunction(x::Int, y::Bool)::String = "$x, $y"
-    #     import ..testfunc
-    #     testfunc(::Bool) = 1
+        # subfunction(x::Int, y::Bool)::String = "$x, $y"
+        # import ..testfunc
+        # testfunc(::Bool) = 1
 
     # end
 
 end
 
-report(io, o::OpaqueMethod) = begin
-    f, m = o.f, o.m
-
-    mt = typeof(f).name.mt
-    name = mt.name
-    hasname = isdefined(mt.module, name) &&
-              typeof(getfield(mt.module, name)) <: Function
-    sname = string(name)
-    kind = if hasname
-        (startswith(sname, '@') ?
-            "macro \"$sname\""
-        : mt.module === Core && m.sig === Tuple ?
-            "builtin function \"$sname\""
-        : # else
-            "generic function \"$sname\"")
-    elseif '#' in sname
-        "anonymous function \"$sname\""
-    elseif mt === Base._TYPE_NAME.mt
-        "type constructor"
-    else
-        "callable object"
+baseShowMethodCustom(io::IO, m::Method, kind::String) = begin
+    tv, decls, file, line = Base.arg_decl_parts(m)
+    sig = Base.unwrap_unionall(m.sig)
+    if sig === Tuple
+        # Builtin
+        print(io, m.name, "(...)  [", kind, " @ ", m.module, "]")
+        return
     end
-    println(io, "$m (method for $kind)")
+    print(io, decls[1][2], "(")
+    join(
+        io,
+        String[isempty(d[2]) ? d[1] : string(d[1], "::", d[2]) for d in decls[2:end]],
+        ", ",
+        ", ",
+    )
+    kwargs = Base.kwarg_decl(m)
+    if !isempty(kwargs)
+        print(io, "; ")
+        join(io, map(Base.sym_to_string, kwargs), ", ", ", ")
+    end
+    print(io, ")")
+    Base.show_method_params(io, tv)
+
+    print(io, "  [", kind, " @ ", m.module)
+    if line > 0
+        file, line = Base.updated_methodloc(m)
+        print(io, " ", file, ":", line)
+    end
+    print(io, "]")
+end
+
+baseShowTypeCustom(io::IO, @nospecialize(x::Type)) = begin
+    if !Base.print_without_params(x)
+        properx = Base.makeproper(io, x)
+        if Base.make_typealias(properx) !== nothing || (Base.unwrap_unionall(x) isa Union && x <: Base.make_typealiases(properx)[2])
+            # show(IOContext(io, :compact => true), x)
+            if !(get(io, :compact, false)::Bool)
+                Base.printstyled(IOContext(io, :compact => false), x)
+            end
+            return
+        end
+    end
+    show(io, x)
+end
+
+Base.show(io::IO, d::DiscoveredMacro) = begin
+    print(io, "function ")
+    baseShowMethodCustom(io, d.m, "macro")
+end
+
+Base.show(io::IO, d::DiscoveredLambda) = begin
+    print(io, "function ")
+    baseShowMethodCustom(io, d.m, "lambda")
+end
+
+Base.show(io::IO, d::DiscoveredConstructor) = begin
+    print(io, "function ")
+    baseShowMethodCustom(io, d.m, "constructor")
+end
+
+Base.show(io::IO, d::DiscoveredCallable) = begin
+    print(io, "function ")
+    baseShowMethodCustom(io, d.m, "callable")
+end
+
+Base.show(io::IO, d::DiscoveredBuiltin) = begin
+    print(io, "function ")
+    baseShowMethodCustom(io, d.m, "builtin")
+end
+
+Base.show(io::IO, d::DiscoveredIntrinsic) = begin
+    print(io, "function ")
+    baseShowMethodCustom(io, d.m, "intrinsic")
+end
+
+Base.show(io::IO, d::DiscoveredGeneric) = begin
+    print(io, "function ")
+    baseShowMethodCustom(io, d.m, "generic")
+end
+
+Base.show(io::IO, d::DiscoveredAlias) = begin
+    print(io, "const $(d.m).$(d.s) = ")
+    baseShowTypeCustom(io, d.t)
+end
+
+Base.show(io::IO, d::DiscoveredClosure) = begin
+    show(io, DiscoveredType(d.m, d.s, d.t))
+end
+
+Base.show(io::IO, d::DiscoveredFunctionType) = begin
+    show(io, d.t)
+end
+
+Base.show(io::IO, d::DiscoveredType) = begin
+    print(io,
+        if isabstracttype(d.t)
+            "abstract type"
+        elseif isstructtype(d.t)
+            ismutabletype(d.t) ? "mutable struct" : "struct"
+        elseif isprimitivetype(d.t)
+            "primitive type"
+        else
+            "???"
+        end)
+    print(io, " ", Base.unwrap_unionall(d.t))
+    if supertype(d.t) !== Any
+        print(io, " <: ")
+        b = Base.unwrap_unionall(supertype(d.t))
+        Base.show_type_name(io, b.name)
+        isempty(b.parameters) || print(io, "{")
+        print(io, join(map(p -> p isa TypeVar ? p.name : p, b.parameters), ", "))
+        isempty(b.parameters) || print(io, "}")
+    end
+    if isprimitivetype(d.t)
+        print(io, " ", 8 * sizeof(d.t))
+    end
+    print(io, " end")
 end
 
 
@@ -408,60 +587,48 @@ const Abstract
 
 =#
 
-report(io, o::OpaqueType) = begin
-    @assert (o.t isa DataType) || (o.t isa UnionAll)
+# builtins(mod) = filter(n -> getproperty(mod, n) isa Core.Builtin, names(mod; all=true, imported=true))
+# intrinsics(mod) = filter(n -> getproperty(mod, n) isa Core.IntrinsicFunction, names(mod; all=true, imported=true))
 
-    typevars(t) = begin
-        res = TypeVar[]
-        while t isa UnionAll
-            push!(res, t.var)
-            t = t.body
-        end
-        res
-    end
+discoverDeclarationsAndSignatures(mods::AbstractVector{Module}=Base.loaded_modules_array();
+        funcfile=nothing,
+        typefile=nothing,
+        skip_macros=true,
+        skip_lambdas=true,
+        skip_constructors=false,
+        skip_callable=false,
+        skip_builtins=true,
+        skip_intrinsics=true,
+        skip_generics=false,
+        skip_aliases=true,
+        skip_closures=true,
+        skip_functiontypes=true,
+        skip_types=false) = begin
 
-    sym, typ, mod = o.s, o.t, o.m
-    base = Base.unwrap_unionall(typ)
+    funio = isnothing(funcfile) ? Base.stdout : open(funcfile, "w")
+    typio = isnothing(typefile) ? Base.stdout : open(typefile, "w")
 
-    if base isa Union || typ !== base.name.wrapper || string(sym) != string(nameof(typ))
-        vars = typevars(typ)
-        name = "$(mod).$(sym)"
-        println(io, "const $(name)$(isempty(vars) ? "" : "{$(join(vars, ", "))}") = $(base) (from module $(mod))")
-    else
-        try
-            kind = if isabstracttype(typ)
-                "abstract type"
-            elseif isstructtype(typ)
-                ismutabletype(typ) ? "mutable struct" : "struct"
-            elseif isprimitivetype(typ)
-                "primitive type"
-            else
-                "???"
-            end
-            name = "$(base)"
-            super = supertype(typ) === Any ? "" : " <: $(Base.unwrap_unionall(supertype(typ)))"
-            size = isprimitivetype(typ) ? " $(8 * sizeof(typ))" : ""
-            closure = (typ <: Function && '#' in name) ? "(closure) " : ""
+    shouldShow(::DiscoveredMacro) = !skip_macros
+    shouldShow(::DiscoveredLambda) = !skip_lambdas
+    shouldShow(::DiscoveredConstructor) = !skip_constructors
+    shouldShow(::DiscoveredCallable) = !skip_callable
+    shouldShow(::DiscoveredBuiltin) = !skip_builtins
+    shouldShow(::DiscoveredIntrinsic) = !skip_intrinsics
+    shouldShow(::DiscoveredGeneric) = !skip_generics
+    shouldShow(::DiscoveredAlias) = !skip_aliases
+    shouldShow(::DiscoveredClosure) = !skip_closures
+    shouldShow(::DiscoveredFunctionType) = !skip_functiontypes
+    shouldShow(::DiscoveredType) = !skip_types
 
-            println(io, "$(closure)$(kind) $(name)$(super)$(size) end (from module $(mod))")
-        catch e
-            println(io, "ERROR: $o")
-        end
-    end
-end
-
-dumpDeclarationsAndSignatures(console::Bool, testing::Bool) = begin
-    mods = testing ? [TestMod] : Base.loaded_modules_array()
-    funio = console ? Base.stdout : open("functions.jlg", "w")
-    typio = console ? Base.stdout : open("types.jlg", "w")
     try
-        declarationsAndSignatures(mods) do x
-            x isa OpaqueMethod && report(funio, x)
-            x isa OpaqueType && report(typio, x)
+        discover(mods) do x
+            shouldShow(x) || return
+            io = x isa FunctionDiscovery ? funio : typio
+            println(io, x)
         end
     finally
-        console || close(funio)
-        console || close(typio)
+        funio === Base.stdout || close(funio)
+        typio === Base.stdout || close(typio)
     end
 end
 
